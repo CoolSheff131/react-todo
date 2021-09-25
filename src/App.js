37376cd9dc5677342cfcd47f4ef24861723a5ad5
Александр Никульшеев <a.nikulsheev@gmail.com>
2021-09-25 11:28:35 +0800

    Добавлено выведение элементов списка todos

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import TodoList from './Todo/TodoList';
 function App() {
+  const todos = [
+    {id: 1, completed: false, title: 'Buy bread'},
+    {id: 2, completed: false, title: 'Buy butter'},
+    {id: 3, completed: false, title: 'Buy milk'}
+]    
   return (
     <div className="wrapper">
       <h1> React TODO</h1>
-      <TodoList></TodoList>
+      <TodoList todos={todos}></TodoList>
     </div>
   );
 }
